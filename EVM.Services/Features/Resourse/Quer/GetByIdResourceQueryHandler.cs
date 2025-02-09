c346c3bd5952ab3f226c6580bea85fc286835047
kreycher <161151306+kreycher@users.noreply.github.com>
2025-02-09 23:39:38 +0100

    Added GetByIdResource and GetResource.

---
+++ EVM.Services/Features/Resourse/Quer/GetByIdResourceQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using EVM.Data;
+using EVM.Services.Exceptions;
+using EVM.Services.Extensions;
+using EVM.Services.Features.Event.Models.Responses;
+using EVM.Services.Features.Models.Responses;
+using EVM.Services.Features.Resourse.Models.Responses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace EVM.Services.Features.Resourse.Query;
+public class GetByIdResourceQueryHandler
+ (ILogger<GetByIdResourceQueryHandler> _logger, AppDbContext _appDbContext, IHttpContextAccessor _httpContextAccessor, IAuthorizationService _authorizationService)
+{
+
+    private readonly HttpContext _httpContext = _httpContextAccessor.HttpContext ?? throw new MissingHttpContextException();
+    public async Task<ApiResponse<GetEventResponse>> Handle(Guid id, CancellationToken cancellationToken)
+    {
+        await _authorizationService.CanPerformActionAsync(_httpContext.User, "Read", "Resource");
+        var userId = _httpContext.User.GetId() ?? throw new UserNotFoundException();
+        var events = await _appDbContext.Events.Where(x => x.UserId == userId && x.Id == id)
+            .Select(x => new GetEventResponse
+            {
+                ETask = x.EventTasks.ToList(),
+                Name = x.Title,
+                Description = x.Description,
+            })
+            .FirstOrDefaultAsync(cancellationToken);
+        return new(events!);
+    }
+}
---
+++ EVM.Services/Features/Resourse/Quer/GetResourceQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using EVM.Data;
+using EVM.Services.Exceptions;
+using EVM.Services.Extensions;
+using EVM.Services.Features.Event.Models.Responses;
+using EVM.Services.Features.Models.Responses;
+using EVM.Services.Features.Resourse.Models.Responses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace EVM.Services.Features.Resourse.Query;
+
+public class GetResourceQueryHandler
+(ILogger<GetResourceQueryHandler> _logger, AppDbContext _appDbContext, IHttpContextAccessor _httpContextAccessor, IAuthorizationService _authorizationService)
+{
+    private readonly HttpContext _httpContext = _httpContextAccessor.HttpContext ?? throw new MissingHttpContextException();
+    public async Task<ApiResponse<List<GetEventResponse>>> Handle(CancellationToken cancellationToken)
+    {
+        await _authorizationService.CanPerformActionAsync(_httpContext.User, "Read", "Resource");
+        var userId = _httpContext.User.GetId() ?? throw new UserNotFoundException();
+        var events = await _appDbContext.Events
+            .Where(x => x.UserId == userId)
+            .Select(x => new GetEventResponse
+            {
+                ETask = x.EventTasks.ToList(),
+                Name = x.Title,
+                Description = x.Description,
+            })
+            .ToListAsync(cancellationToken);
+        return new(events);
+    }
+}
